375335f0dfed3605c87281689ecc0cec752b45af
alec <alec.hulce@gmail.com>
2014-04-01 23:34:59 -0700

    u

--- index.js
+++ index.js
@@ -3,9 +3,7 @@ module.exports = sext;
 
 function sext(/*deep,o1,o2,etc*/){
   var deep = arguments[0] === true
-    ,o1 = arguments[deep?1:0]
-    ,o2,i
-  ;
+    ,o1 = arguments[deep?1:0],o2,i
   if (!(o1 instanceof Object))
     o1 = {};
   for (i=deep?2:1;i<arguments.length;++i) {
@@ -13,10 +11,7 @@ function sext(/*deep,o1,o2,etc*/){
     if (!(o2 instanceof Object))
       continue;
     Object.keys(o2).forEach(function(k){
-      if (deep)
-        o1[k] = o2[k] instanceof Object ? sext(true,o1[k],o2[k]) : o2[k];
-      else
-        o1[k] = o2[k];
+      o1[k] = deep && o2[k] instanceof Object ? sext(true,o1[k],o2[k]) : o2[k];
     });
   }
   return o1;
